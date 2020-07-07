93bea5629436177b51211e33e8ef1d11cd2882d9
aaronfraint <afraint@dvrpc.org>
2020-07-07 14:49:29 -0700

    download data and create a SQL file

--- sidewalk_gaps/main.py
+++ sidewalk_gaps/main.py
@@ -1,7 +1,7 @@
 import postgis_helpers as pGIS
 from postgis_helpers import PostgreSQL
 
-from sidewalk_gaps._download_data import copy_production_data
+from sidewalk_gaps.download_data import copy_production_data
 
 local_db_credentials = pGIS.configurations()["localhost"]
 dvrpc_db_credentials = pGIS.configurations()["dvrpc_gis"]
@@ -12,6 +12,7 @@ remote_db = PostgreSQL("gis", **dvrpc_db_credentials)
 
 if __name__ == "__main__":
     # Copy the data to a local DB
-    # copy_production_data(remote_db, local_db)
+    copy_production_data(remote_db, local_db)
 
+    # Save the SQL database to file with pg_dump
     local_db.db_export_pgdump_file()
