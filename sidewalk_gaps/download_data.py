adc2a9bf498b7549ceb63dfed2f748493364004d
aaronfraint <afraint@dvrpc.org>
2020-07-08 08:40:26 -0700

    roll the input database

--- sidewalk_gaps/download_data.py
+++ sidewalk_gaps/download_data.py
@@ -17,11 +17,11 @@ Usage
 See ``main.py``
 
 """
-
+from pathlib import Path
 from postgis_helpers import PostgreSQL
 
 
-def copy_production_data(remote_db: PostgreSQL, local_db: PostgreSQL):
+def import_production_sql_data(remote_db: PostgreSQL, local_db: PostgreSQL):
     """
     Copy data from DVRPC's production SQL database to a SQL database on localhost.
 
@@ -32,22 +32,67 @@ def copy_production_data(remote_db: PostgreSQL, local_db: PostgreSQL):
         ("transportation", ["pedestriannetwork_lines",
                             "pedestriannetwork_points",
                             "pa_centerline",
-                            "nj_centerline",
                             ]),
 
         ("structure", ["points_of_interest"]),
+
+        ("boundaries", ["municipalboundaries"]),
     ]
 
     for schema, table_list in data_to_download:
 
         for table_name in table_list:
 
+            # Extract the data from the remote database and rename the geom column
             query = f"SELECT * FROM {schema}.{table_name};"
             gdf = remote_db.query_as_geo_df(query, geom_col="shape")
             gdf = gdf.rename(columns={"shape": "geometry"}).set_geometry("geometry")
 
+            # Check if we have multipart geometries.
+            # If so, explode them (but keep the index)
+            multipart = False
+            for geom_type in gdf.geom_type.unique():
+                if "Multi" in geom_type:
+                    multipart = True
+
+            if multipart:
+                print(f"EXPLODING {table_name}")
+                gdf = gdf.explode()
+                gdf['explode'] = gdf.index
+                gdf = gdf.reset_index()
+
+            # Save to the local database
             local_db.import_geodataframe(gdf, table_name)
+
+    # Custom query is needed for nj_centerline
+    nj_centerline_query = """
+        SELECT *, (ST_DUMP(shape)).geom
+        FROM transportation.nj_centerline
+    """
+
+    nj_gdf = remote_db.query_as_geo_df(nj_centerline_query)
+
+    # Remove 'nan's
+    for idx, row in nj_gdf.iterrows():
+        geom_txt = str(row["geom"])
+        if "nan" in geom_txt:
+            nj_gdf = nj_gdf.drop(idx)
+
+    local_db.import_geodataframe(nj_gdf, "nj_centerlines")
+
+    if "nj_centerline" not in local_db.all_spatial_tables_as_dict():
+        print("ERROR! nj_centerline didn't make it! ")
+
+
+def import_shapefiles(folder: Path, db: PostgreSQL):
+
+    for shp_path in folder.rglob("*.shp"):
+
+        pg_name = shp_path.name[:-4].replace(" ", "_").lower()
+
+        db.import_geodata(pg_name, shp_path, if_exists="replace")
 
 
 if __name__ == "__main__":
+
     pass
--- sidewalk_gaps/main.py
+++ sidewalk_gaps/main.py
@@ -1,7 +1,8 @@
 import postgis_helpers as pGIS
 from postgis_helpers import PostgreSQL
 
-from sidewalk_gaps.download_data import copy_production_data
+from sidewalk_gaps import FOLDER_SHP_INPUT
+from sidewalk_gaps.download_data import import_production_sql_data, import_shapefiles
 
 local_db_credentials = pGIS.configurations()["localhost"]
 dvrpc_db_credentials = pGIS.configurations()["dvrpc_gis"]
@@ -12,7 +13,10 @@ remote_db = PostgreSQL("gis", **dvrpc_db_credentials)
 
 if __name__ == "__main__":
     # Copy the data to a local DB
-    copy_production_data(remote_db, local_db)
+    import_production_sql_data(remote_db, local_db)
+
+    # Import the shapefiles in the input folder
+    import_shapefiles(FOLDER_SHP_INPUT, local_db)
 
     # Save the SQL database to file with pg_dump
     local_db.db_export_pgdump_file()
