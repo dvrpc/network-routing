e906620c8b9d969ae631f1cdd5911266d41e8534
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-07-10 10:46:59 -0700

    documentation!

--- sidewalk_gaps/network_analysis.py
+++ sidewalk_gaps/network_analysis.py
@@ -6,8 +6,6 @@ import pandas as pd
 from shapely.geometry import LineString
 from geoalchemy2 import Geometry, WKTElement
 
-WALKING_SPEED_MPH = 2.5
-
 
 class SidewalkNetwork:
 
@@ -15,15 +13,40 @@ class SidewalkNetwork:
                  db: PostgreSQL,
                  schema: str,
                  walking_mph: float = 2.5,
-                 max_minutes: float = 60.0):
+                 max_minutes: float = 60.0,
+                 epsg: int = 26918,
+                 edge_table_name: str = "pedestriannetwork_lines",
+                 run_on_create: bool = True):
 
+        # Capture user input
         self.db = db
         self.schema = schema
         self.walking_mph = walking_mph
         self.max_minutes = max_minutes
-        self.themes = self.get_themes()
+        self.epsg = epsg
+        self.edge_table_name = edge_table_name
 
-    def get_themes(self):
+        # Get a list of POI themes within the schema
+        self.themes = self._get_themes()
+
+        # Set the SQL database schema to the active schema
+        self.db.ACTIVE_SCHEMA = schema
+
+        if run_on_create:
+            self.setup()
+            self.construct_network()
+            self.analyze_pois()
+
+    def _get_themes(self):
+        """
+        Return a dictionary with each unique POI theme within the schema.
+
+        Key is the raw text from SQL, value is the santinized table name
+
+            ```{"Food/Drink": "fooddrink",
+                "Library"   : "library",}```
+
+        """
         theme_query = f"""
             SELECT distinct theme
             FROM {self.schema}.points_of_interest
@@ -37,48 +60,29 @@ class SidewalkNetwork:
 
         return themes
 
-    def _prep_raw_geom_table(self,
-                             table_name: str,
-                             geom_type: str,
-                             epsg: int = 26918,
-                             id_col: str = "uid"):
-        
-        uid_query = f"""
-            ALTER TABLE {self.schema}.{table_name} ADD {id_col} serial PRIMARY KEY;
-        """
-
-        index_query = f"""
-            CREATE INDEX ON {self.schema}.{table_name} USING GIST (geom);
-        """
-
-        update_geom_table = f"""
-            ALTER TABLE {self.schema}.{table_name}
-            ALTER COLUMN geom TYPE geometry({geom_type}, {epsg})
-            USING ST_Transform(ST_SetSRID(geom, {epsg}), {epsg});
-        """
-
-        for query in [uid_query, index_query, update_geom_table]:
-            self.db.execute(query)
-
-    def _make_geotable_from_query(self, query, **kwargs):
-        self.db.execute(query)
-        self._prep_raw_geom_table(**kwargs)
-
-    # Set up the analysis by: 
-    # 1) making nodes
-    # 2) assigning node ids to the edge network
-    # 3) adding travel time weights (walking)
+    # PREPARE THE NETWORK FOR ANALYSIS
+    # --------------------------------
 
     def setup(self):
+        """
+        Set up the analysis by:
+            1) making nodes
+            2) assigning node ids to the edge network
+            3) adding travel time weights (walking)
+        """
         self.make_nodes()
         self.assign_node_ids_to_network()
         self.add_travel_time_weights()
 
     def make_nodes(self):
+        """
+        Use the edge table to generate a set of nodes.
+
+        These nodes are all of the unique start and 
+        endpoints of the line segments.
+        """
 
         query = f"""
-            DROP TABLE IF EXISTS {self.schema}.nodes;
-            CREATE TABLE {self.schema}.nodes AS
             SELECT st_startpoint(geom) AS geom
             FROM {self.schema}.pedestriannetwork_lines
             UNION
@@ -87,22 +91,27 @@ class SidewalkNetwork:
             GROUP BY geom
         """
 
-        self._make_geotable_from_query(query,
-                                       table_name="nodes",
-                                       geom_type="Point",
-                                       id_col="node_id")
+        self.db.make_geotable_from_query(query,
+                                         new_table_name="nodes",
+                                         geom_type="Point",
+                                         epsg=self.epsg,
+                                         uid_col="node_id")
 
     def assign_node_ids_to_network(self):
+        """
+        Using the newly-created node table, we now
+        need to assign two node_id values to each
+        segment: one at the start, one at the end.
+        """
 
+        # Add columns for the start and end node_id values
         for column_name in ["start_id", "end_id"]:
-            query = f"""
-                ALTER TABLE {self.schema}.pedestriannetwork_lines
-                ADD COLUMN {column_name} INT;
-            """
-            self.db.execute(query)
+            self.db.table_add_or_nullify_column(self.edge_table_name,
+                                                column_name, "INT")
 
+        # Execute the query for the START of each segment
         start_id_query = f"""
-            UPDATE {self.schema}.pedestriannetwork_lines ln
+            UPDATE {self.schema}.{self.edge_table_name} ln
             SET start_id = (SELECT pt.node_id
                             FROM {self.schema}.nodes pt
                             WHERE ST_DWITHIN(pt.geom, st_startpoint(ln.geom), 5)
@@ -110,46 +119,52 @@ class SidewalkNetwork:
                                                 st_startpoint(ln.geom))
                                     ASC LIMIT 1)
         """
-
         self.db.execute(start_id_query)
 
+        # Execute the query for the END of each segment
         end_id_query = start_id_query.replace("start", "end")
-
         self.db.execute(end_id_query)
 
     def add_travel_time_weights(self):
+        """
+        Add impedence columns to the edges.
+
+            1) Calculate the legnth in meters (epsg:26918)
+            2) Convert meters into minutes:
+                - divide by 1609.34
+                - divide by defined walking speed in MPH
+                - multiply by 60 to get minutes
+        """
 
         # Add a meter length and minutes column
         for column_name in ["len_meters", "minutes"]:
-            query = f"""
-                ALTER TABLE {self.schema}.pedestriannetwork_lines
-                ADD COLUMN {column_name} FLOAT;
-            """
-            self.db.execute(query)
+            self.db.table_add_or_nullify_column(self.edge_table_name,
+                                                column_name, "FLOAT")
 
         # Capture length in meters into its own column
         update_meters = f"""
-            UPDATE {self.schema}.pedestriannetwork_lines
+            UPDATE {self.schema}.{self.edge_table_name}
             SET len_meters = ST_LENGTH(geom);
         """
         self.db.execute(update_meters)
 
         # Calculate walking travel time in minutes
         update_minutes = f"""
-            UPDATE {self.schema}.pedestriannetwork_lines
+            UPDATE {self.schema}.{self.edge_table_name}
             SET minutes = len_meters / 1609.34 / {self.walking_mph} * 60;
         """
         self.db.execute(update_minutes)
 
-    # Build the network with Pandana
+    # TURN THE POSTGIS DATA INTO A PANDANA NETWORK
+    # --------------------------------------------
 
     def construct_network(self):
 
-        # EDGES
-        query = f"SELECT * FROM {self.schema}.pedestriannetwork_lines"
+        # Get all edges
+        query = f"SELECT * FROM {self.schema}.{self.edge_table_name}"
         edge_gdf = self.db.query_as_geo_df(query)
 
-        # NODES
+        # Get all nodes
         node_query = f"""
             SELECT node_id,
                 ST_X(st_transform(geom, 4326)) as x,
@@ -159,7 +174,7 @@ class SidewalkNetwork:
         """
         node_gdf = self.db.query_as_geo_df(node_query)
 
-        # Force the ID columns in the EDGE gdf to integer
+        # Force the ID columns in the edge gdf to integer
         for col in ["start_id", "end_id"]:
             edge_gdf[col] = edge_gdf[col].astype(int)
 
@@ -180,52 +195,33 @@ class SidewalkNetwork:
         self.edge_gdf = edge_gdf
         self.node_gdf = node_gdf
 
-    # Add and analyze access to points of interest
-    # --------------------------------------------
+    # ANALYZE POINTS OF INTEREST
+    # --------------------------
 
-    def add_pois_to_network(self):
+    def analyze_pois(self):
+        """
+        For each theme in the POI table:
+            1) Run ``calculate_single_poi()``
+            2) Add a QAQC table showing the POI->Node assignment
+        """
         for theme in self.themes:
             poi_gdf, _ = self.calculate_single_poi(theme)
             self.qaqc_poi_assignment(theme, poi_gdf)
 
-    def qaqc_poi_assignment(self, theme: str, poi_gdf: gpd.GeoDataFrame):
-
-        # Save the network assignment for QAQC
-        poi_gdf["node_id"] = self.network.get_node_ids(poi_gdf["x"],
-                                                       poi_gdf["y"],
-                                                       mapping_distance=1)
-        poi_node_pairs = pd.merge(poi_gdf,
-                                  self.node_gdf,
-                                  left_on='node_id',
-                                  right_on='node_id',
-                                  how='left',
-                                  sort=False,
-                                  suffixes=['_from', '_to'])
-
-        poi_node_pairs["flow"] = [
-            LineString([row["geom_from"], row["geom_to"]]) for idx, row in poi_node_pairs.iterrows()
-        ]
-        poi_node_pairs = poi_node_pairs.set_geometry("flow")
-
-        poi_node_pairs['geom'] = poi_node_pairs["flow"].apply(
-                                    lambda x: WKTElement(x.wkt, srid=26918)
-        )
-
-        for col in ["flow", "geom_from", "geom_to"]:
-            poi_node_pairs.drop(col, inplace=True, axis=1)
-
-        engine = create_engine(self.db.uri())
-        poi_node_pairs.to_sql(f"poi_{self.themes[theme]}_qa",
-                              engine,
-                              schema=self.schema,
-                              dtype={'geom': Geometry("LineString", srid=26918)})
-        engine.dispose()
-
-        self.poi_gdf = poi_gdf
-
     def calculate_single_poi(self, this_theme: str):
+        """
+        1) Add all POIs for a given theme to the network.
+        2) Calculate the minutes away for the 5 nearest POIs of this theme.
+        3) Save the result to non-spatial SQL table
+        4) Run ``add_geometries_to_accessibilty_result()``
+
+        Result
+        ------
+            New table in DB. Each row is a node, and each column
+            has the time to walk to the N-nearest POI.
+        """
+
         nice_theme = self.themes[this_theme]
-        print(nice_theme)
 
         # POIS
         poi_query = f"""
@@ -256,23 +252,26 @@ class SidewalkNetwork:
 
         result_matrix = result_matrix.rename(index=str, columns=new_colnames)
 
-        engine = create_engine(self.db.uri())
-        result_matrix.to_sql(
-            f"poi_{nice_theme}",
-            engine,
-            schema=self.schema,
-            if_exists="replace"
-        )
-        engine.dispose()
+        self.db.import_dataframe(result_matrix, f"poi_{nice_theme}")
 
         self.add_geometries_to_accessibilty_result(f"poi_{nice_theme}")
 
         return poi_gdf, result_matrix
 
     def add_geometries_to_accessibilty_result(self, table_name: str):
+        """
+        Spatialize the non-spatial POI result table with node geometries
+        """
 
         add_geom_col = f"""
-            SELECT AddGeometryColumn('{self.schema}', '{table_name}', 'geom', 26918, 'POINT', 2);
+            SELECT AddGeometryColumn(
+                '{self.schema}',
+                '{table_name}',
+                'geom',
+                {self.epsg},
+                'POINT',
+                2
+            );
         """
         self.db.execute(add_geom_col)
 
@@ -283,20 +282,59 @@ class SidewalkNetwork:
         """
         self.db.execute(update_geom_col)
 
+        # Register node_id as the primary key for the table
         add_primary_key = f"""
             ALTER TABLE {self.schema}.{table_name}
             ADD PRIMARY KEY (node_id);
         """
         self.db.execute(add_primary_key)
 
-    # QAQC!!!
-    # -------
-    # def qaqc_poi_assignment(self, this_theme: str):
+    def qaqc_poi_assignment(self, theme: str, poi_gdf: gpd.GeoDataFrame):
+        """
+        For a given poi geodataframe:
+            1) Get the ID of the nearest node for each POI
+            2) Add a 'flow' geometry from the POI to the assigned node
+            3) Delete all other geometries in the table and write to SQL
+        """
+
+        # Save the network assignment for QAQC
+        poi_gdf["node_id"] = self.network.get_node_ids(poi_gdf["x"],
+                                                       poi_gdf["y"],
+                                                       mapping_distance=1)
+        poi_node_pairs = pd.merge(poi_gdf,
+                                  self.node_gdf,
+                                  left_on='node_id',
+                                  right_on='node_id',
+                                  how='left',
+                                  sort=False,
+                                  suffixes=['_from', '_to'])
+
+        poi_node_pairs["flow"] = [
+            LineString([row["geom_from"], row["geom_to"]]) for idx, row in poi_node_pairs.iterrows()
+        ]
+        poi_node_pairs = poi_node_pairs.set_geometry("flow")
+
+        poi_node_pairs['geom'] = poi_node_pairs["flow"].apply(
+                                    lambda x: WKTElement(x.wkt, srid=self.epsg)
+        )
+
+        for col in ["flow", "geom_from", "geom_to"]:
+            poi_node_pairs.drop(col, inplace=True, axis=1)
+
+        engine = create_engine(self.db.uri())
+        poi_node_pairs.to_sql(f"poi_{self.themes[theme]}_qa",
+                              engine,
+                              schema=self.schema,
+                              dtype={'geom': Geometry("LineString", srid=self.epsg)})
+        engine.dispose()
+
+        self.poi_gdf = poi_gdf
+
 
 if __name__ == "__main__":
     from sidewalk_gaps import CREDENTIALS
 
-    schema = "nj"
+    schema = "camden"
 
     db = PostgreSQL(
         "sidewalk_gaps",
@@ -305,6 +343,3 @@ if __name__ == "__main__":
     )
 
     network = SidewalkNetwork(db, schema)
-    network.setup()
-    network.construct_network()
-    network.add_pois_to_network()
