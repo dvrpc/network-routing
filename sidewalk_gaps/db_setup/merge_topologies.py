6b0ef0ab741c4e998af030ab9b3126c6071bd35c
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-08-31 16:09:23 -0400

    begin working on merging trails w sidewalks

---
+++ sidewalk_gaps/db_setup/merge_topologies.py
@@ -0,0 +1,6 @@
+"""
+This script merges the sidewalk line geometries and CircuitTrails layers
+into a single topologically-sound layer. It requires splitting segments
+within each layer wherever there is an intersection between the two layers.
+"""
+
