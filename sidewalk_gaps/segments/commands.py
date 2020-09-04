2a4cb9560aa64837e6a3a045e2ba5348515194b2
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-09-04 15:07:02 -0400

    Add script to make connected islands

--- sidewalk_gaps/segments/commands.py
+++ sidewalk_gaps/segments/commands.py
@@ -4,6 +4,7 @@ from postgis_helpers import PostgreSQL
 
 from sidewalk_gaps import CREDENTIALS, PROJECT_DB_NAME
 from .centerline_sidewalk_coverage import classify_centerlines
+from .generate_islands import generate_islands
 
 
 @click.command()
@@ -17,3 +18,16 @@ def analyze_segments(schema: str, database):
     """ Classify centerlines w/ length of parallel sidewalks """
     db = PostgreSQL(database, verbosity="minimal", **CREDENTIALS["localhost"])
     classify_centerlines(db, schema, "centerlines")
+
+
+@click.command()
+@click.argument("schema")
+@click.option(
+    "--database", "-d",
+    help="Name of the local database",
+    default=PROJECT_DB_NAME,
+)
+def identify_islands(schema: str, database):
+    """ Join intersecting sidewalks to create 'islands' """
+    db = PostgreSQL(database, verbosity="minimal", **CREDENTIALS["localhost"])
+    generate_islands(db, schema)
---
+++ sidewalk_gaps/segments/generate_islands.py
@@ -0,0 +1,23 @@
+from postgis_helpers import PostgreSQL
+
+
+def generate_islands(db: PostgreSQL, schema: str):
+    """ Use the sidewalk layer to merge intersecting geometries.
+        The output is a layer with one feature per 'island' """
+
+    query = f"""
+        SELECT
+            ST_COLLECTIONEXTRACT(
+                UNNEST(ST_CLUSTERINTERSECTING(geom)),
+                2
+            ) AS geom
+        FROM {schema}.sidewalks
+    """
+    db.make_geotable_from_query(
+        query,
+        "islands",
+        "MULTILINESTRING",
+        26918,
+        schema=schema
+    )
+
