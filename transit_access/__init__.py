bbb85b481eff8f3d7c278f715ab26417e9d8ec25
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-10-20 16:19:03 -0400

    define db once

--- transit_access/__init__.py
+++ transit_access/__init__.py
@@ -1,6 +1,10 @@
 from pathlib import Path
 
-from sidewalk_gaps import GDRIVE_ROOT
+from postgis_helpers import PostgreSQL
 
+from sidewalk_gaps import GDRIVE_ROOT, CREDENTIALS, PROJECT_DB_NAME
 
+# Change Google Drive path to 'RideScore'
 GDRIVE_ROOT = Path(str(GDRIVE_ROOT).replace("Sidewalk Gaps", "RideScore"))
+
+db = PostgreSQL(PROJECT_DB_NAME, **CREDENTIALS["localhost"], verbosity="minimal")
