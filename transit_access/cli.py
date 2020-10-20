3da3d835de0164d95990582f9c086d58c26a8098
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-10-20 16:19:31 -0400

    calculate network distance and isochrones

--- transit_access/cli.py
+++ transit_access/cli.py
@@ -2,26 +2,66 @@ import click
 
 from postgis_helpers import PostgreSQL
 
-from transit_access import GDRIVE_ROOT
+from transit_access import GDRIVE_ROOT, db
 
 from sidewalk_gaps.db_setup.db_setup import import_shapefiles
-from sidewalk_gaps import CREDENTIALS, PROJECT_DB_NAME
+
+from transit_access.ridescore_isochrones import calculate_sidewalk_walksheds, generate_isochrones
 
 
 @click.group()
 def main():
+    "The command 'transit' is used for the RideScore project update."
     pass
 
 
 @click.command()
 def import_data():
     """Import RideScore data into the SQL database"""
-    db = PostgreSQL(PROJECT_DB_NAME, **CREDENTIALS["localhost"])
     ridescore_inputs = GDRIVE_ROOT / "inputs"
     import_shapefiles(ridescore_inputs, db)
 
 
-main.add_command(import_data)
+@click.command()
+def data_engineering():
+    """Massage the data to prepare for analysis"""
+
+    for schema, state_name in [("nj", "New Jersey"), ("pa", "Pennsylvania")]:
+        query = f"""
+            select * from passengerrailstations p
+            where
+                st_within(
+                    geom,
+                    (select st_collect(geom) from regional_counties
+                        where state_name = '{state_name}')
+                )
+        """
+        db.make_geotable_from_query(
+            query,
+            "ridescore_stations",
+            "POINT",
+            26918,
+            schema=schema
+        )
+
+
+@click.command()
+def calculate_sidewalks():
+    """Analyze sidewalk network distance around each rail stop """
+    calculate_sidewalk_walksheds()
+
+
+@click.command()
+def isochrones():
+    """Turn access results into isochrone polygons"""
+    generate_isochrones()
+
+
+all_commands = [import_data, data_engineering, calculate_sidewalks, isochrones]
+
+for cmd in all_commands:
+    main.add_command(cmd)
+
 
 if __name__ == "__main__":
     pass
---
+++ transit_access/ridescore_isochrones.py
@@ -0,0 +1,67 @@
+import pandas as pd
+
+from transit_access import db
+
+from sidewalk_gaps.accessibility.network_analysis import SidewalkNetwork
+
+
+def calculate_sidewalk_walksheds():
+
+    ridescore_arguments = {
+        "poi_table_name": "ridescore_stations",
+        "poi_id_column": "dvrpc_id",
+        "output_table_name": "ridescore",
+        "num_pois": 1,
+        "poi_match_threshold": 152  # aka 500'
+    }
+
+    for schema in ["nj", "pa"]:
+        print("Calculating sidewalk network distance for:", schema)
+        network = SidewalkNetwork(db, schema, **ridescore_arguments)
+
+
+def generate_isochrones():
+
+    all_results = []
+
+    # Convert 0.5 miles to minutes
+    miles = 0.5
+    time_cutoff = miles * 60 / 2.5
+
+    for schema in ["pa", "nj"]:
+        station_ids = db.query_as_list(
+            f"SELECT distinct dvrpc_id from {schema}.ridescore_stations"
+        )
+
+        for dvrpc_id in station_ids:
+            # Figure out if there's results
+            node_count_query = f"""
+                SELECT COUNT(node_id)
+                FROM {schema}.ridescore_results
+                WHERE n_1_{dvrpc_id} <= {time_cutoff}
+            """
+            node_count = db.query_as_single_item(node_count_query)
+
+            # Only make isochrones if there's nodes below the threshold
+            if node_count > 0:
+                print(f"Isochrone for DVRPC ID#{dvrpc_id} within {schema}")
+
+                query = f"""
+                    select  st_buffer(
+                                st_collectionextract(
+                                    st_concavehull(st_collect(geom), 0.99),
+                                    3),
+                                45) as geom
+                    from {schema}.ridescore_results
+                    where n_1_{dvrpc_id} <= {time_cutoff}
+                """
+                gdf = db.query_as_geo_df(query)
+
+                gdf["schema"] = schema
+                gdf["dvrpc_id"] = dvrpc_id
+
+                all_results.append(gdf)
+
+    merged_gdf = pd.concat(all_results)
+
+    db.import_geodataframe(merged_gdf, "ridescore_isos")
