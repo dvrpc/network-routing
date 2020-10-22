37f1152b3c93423d62b8d30f1c5f16a283da75f1
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-10-22 13:40:42 -0400

    define db creation process once

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,2 +1,18 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv, find_dotenv
+
+import postgis_helpers as pGIS
+
 from .nodes import generate_nodes
-from .import_shapefiles import import_shapefiles
+
+load_dotenv(find_dotenv())
+DB_NAME = os.getenv("DB_NAME")
+DB_HOST = os.getenv("DB_HOST")
+GDRIVE_ROOT = os.getenv("GDRIVE_ROOT")
+
+CREDENTIALS = pGIS.configurations()
+
+
+def db_connection():
+    return pGIS.PostgreSQL(DB_NAME, **CREDENTIALS[DB_HOST])
