c52dbc6e8a55e14521bf67ff9ddf48684dd1b49c
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-10-28 10:56:13 -0400

    allow easy import of RoutableNetwork

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -5,6 +5,7 @@ from dotenv import load_dotenv, find_dotenv
 import postgis_helpers as pGIS
 
 from .nodes import generate_nodes
+from .routable_network import RoutableNetwork
 
 load_dotenv(find_dotenv())
 DB_NAME = os.getenv("DB_NAME")
@@ -15,4 +16,4 @@ CREDENTIALS = pGIS.configurations()
 
 
 def db_connection():
-    return pGIS.PostgreSQL(DB_NAME, **CREDENTIALS[DB_HOST])
+    return pGIS.PostgreSQL(DB_NAME, verbosity="minimal", **CREDENTIALS[DB_HOST])
