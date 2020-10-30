608e97e1aa0d375a167eae85b59d81be22aa84be
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-10-30 15:02:28 -0400

    combine outputs from OSM and SW

--- transit_access/ridescore_isochrones.py
+++ transit_access/ridescore_isochrones.py
@@ -3,37 +3,29 @@ from tqdm import tqdm
 
 from postgis_helpers import PostgreSQL
 
-from helpers import RoutableNetwork
-
-
-# def calculate_sidewalk_walksheds(db: PostgreSQL):
-
-#     ridescore_arguments = {
-#         "poi_table_name": "ridescore_stations",
-#         "poi_id_column": "dvrpc_id",
-#         "output_table_name": "ridescore",
-#         "num_pois": 1,
-#         "poi_match_threshold": 152  # aka 500'
-#     }
-
-#     for schema in ["nj", "pa"]:
-#         print("Calculating sidewalk network distance for:", schema)
-#         network = RoutableNetwork(db, schema, **ridescore_arguments)
-
 
 def generate_isochrones(db: PostgreSQL):
 
-    all_results = []
+    # Make a schema for data viz products
+    sql_query = "CREATE SCHEMA IF NOT EXISTS data_viz;"
+    db.execute(sql_query)
 
-    # Convert 0.5 miles to minutes
-    miles = 0.5
+    # Convert 1 mile to minutes
+    miles = 1
     time_cutoff = miles * 60 / 2.5
 
-    for schema in ["nj", "pa"]:
+    all_results = []
+
+    ridescore_results = [
+        ("rs_osm", "osm_results"),
+        ("rs_sw", "sw_results")
+    ]
+
+    for schema, result_table in ridescore_results:
 
         print(f"Generating isochrone for {schema.upper()}")
 
-        result_cols = db.table_columns_as_list("ridescore_results", schema=schema)
+        result_cols = db.table_columns_as_list(result_table, schema=schema)
 
         station_ids = [x[4:] for x in result_cols if "n_1_" in x]
 
@@ -41,7 +33,7 @@ def generate_isochrones(db: PostgreSQL):
             # Figure out if there's results
             node_count_query = f"""
                 SELECT COUNT(node_id)
-                FROM {schema}.ridescore_results
+                FROM {schema}.{result_table}
                 WHERE n_1_{dvrpc_id} <= {time_cutoff}
             """
             node_count = db.query_as_single_item(node_count_query)
@@ -55,7 +47,7 @@ def generate_isochrones(db: PostgreSQL):
                                     st_concavehull(st_collect(geom), 0.99),
                                     3),
                                 45) as geom
-                    from {schema}.ridescore_results
+                    from {schema}.{result_table}
                     where n_1_{dvrpc_id} <= {time_cutoff}
                 """
                 gdf = db.query_as_geo_df(query)
@@ -69,4 +61,4 @@ def generate_isochrones(db: PostgreSQL):
 
     merged_gdf = pd.concat(all_results)
 
-    db.import_geodataframe(merged_gdf, "ridescore_isos")
+    db.import_geodataframe(merged_gdf, "ridescore_isos", schema="data_viz")
