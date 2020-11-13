7bb6b3e0c8a653eb1b2f0dcd3e3dc8d0ba0156a7
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2020-11-13 16:47:16 -0500

    use IPD data to compare equity to results

--- sidewalk_gaps/data_viz/commands.py
+++ sidewalk_gaps/data_viz/commands.py
@@ -1,30 +1,22 @@
 import click
 
-from postgis_helpers import PostgreSQL
-
-from sidewalk_gaps import CREDENTIALS, PROJECT_DB_NAME
+from helpers import db_connection
 from .hexagon_summary import hexagon_summary, classify_hex_results
 
 
 @click.command()
-@click.option(
-    "--database", "-d",
-    help="Name of the local database",
-    default=PROJECT_DB_NAME,
-)
-def summarize_into_hexagons(database: str):
+def summarize_into_hexagons():
     """ Classify centerlines w/ length of parallel sidewalks """
-    db = PostgreSQL(database, verbosity="minimal", **CREDENTIALS["localhost"])
+
+    db = db_connection()
+
     hexagon_summary(db)
 
 
 @click.command()
-@click.option(
-    "--database", "-d",
-    help="Name of the local database",
-    default=PROJECT_DB_NAME,
-)
-def classify_hexagons(database: str):
+def classify_hexagons():
     """ Prepare analysis result for bivariate choropleth map """
-    db = PostgreSQL(database, verbosity="minimal", **CREDENTIALS["localhost"])
+
+    db = db_connection()
+
     classify_hex_results(db)
