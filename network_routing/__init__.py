ed607543b378aaf66ce2fdd63005deb79e7aee38
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2021-04-19 11:00:45 -0400

    clean up

--- network_routing/__init__.py
+++ network_routing/__init__.py
@@ -1,26 +1,25 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
-
 import postgis_helpers as pGIS
 
-# import network_routing.accessibility as accessibility
 from network_routing.accessibility.routable_network import RoutableNetwork
 
-
+# Load environment variables
 load_dotenv(find_dotenv())
 DB_NAME = os.getenv("DB_NAME")
 DB_HOST = os.getenv("DB_HOST")
 GDRIVE_ROOT = Path(os.getenv("GDRIVE_ROOT"))
 
+# Define downstream folder paths
 FOLDER_DATA_PRODUCTS = GDRIVE_ROOT / "projects/RideScore/data products"
 GDRIVE_SW_GAPS_PROJECT_ROOT = GDRIVE_ROOT / "projects/Sidewalk Gaps"
 
 
-def db_connection():
+def db_connection(database_name: str = DB_NAME, host: str = DB_HOST) -> pGIS.PostgreSQL:
 
     CREDENTIALS = pGIS.configurations()
 
-    db = pGIS.PostgreSQL(DB_NAME, verbosity="minimal", **CREDENTIALS[DB_HOST])
+    db = pGIS.PostgreSQL(database_name, verbosity="minimal", **CREDENTIALS[host])
 
     return db
