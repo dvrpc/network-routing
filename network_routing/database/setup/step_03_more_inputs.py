42f2ec8fb236b3b17c9ec542e02d9c7f23e6bceb
aaronfraint <aaronfraint@icloud.com>
2021-06-21 10:44:03 -0400

    generate county-specific ETA tables

--- network_routing/database/setup/step_03_more_inputs.py
+++ network_routing/database/setup/step_03_more_inputs.py
@@ -1,4 +1,4 @@
-from network_routing import db_connection, GDRIVE_SW_GAPS_PROJECT_ROOT
+from network_routing import pg_db_connection, GDRIVE_SW_GAPS_PROJECT_ROOT
 
 
 def setup_03_import_mode_data():
@@ -7,9 +7,34 @@ def setup_03_import_mode_data():
     """
     shp_path = GDRIVE_SW_GAPS_PROJECT_ROOT / "data-to-import" / "eta_essential_services.shp"
 
-    db = db_connection()
+    db = pg_db_connection()
 
-    db.import_geodata("eta_points", shp_path)
+    db.import_gis(
+        sql_tablename="eta_points", filepath=shp_path, gpd_kwargs={"if_exists": "replace"}
+    )
+
+    # Generate a cut of the ETA points for each of the nine counties
+
+    for county_name in db.query_as_list_of_singletons(
+        "select distinct co_name from regional_counties"
+    ):
+        query = f"""
+            select
+                uid as eta_uid,
+                name, type, geom
+            from
+                eta_points ep
+            where
+                st_within(
+                    geom,
+                    (select st_collect(geom) from regional_counties 
+                     where co_name = '{county_name}')
+                )
+        """
+
+        db.gis_make_geotable_from_query(
+            query, f"eta_{county_name.lower()}", geom_type="Point", epsg=26918
+        )
 
 
 if __name__ == "__main__":
