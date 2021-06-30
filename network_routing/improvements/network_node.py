c5f90799caeb9c77bbe65c88a0bbff34f4c48287
aaronfraint <aaronfraint@icloud.com>
2021-06-29 21:18:11 -0400

    add methods

--- network_routing/improvements/network_node.py
+++ network_routing/improvements/network_node.py
@@ -1,20 +1,51 @@
 """
 """
-from postgis_helpers.PgSQL import PostgreSQL
-
-from network_routing import db_connection
-
-_db = db_connection()
+from network_routing import pg_db_connection
+from pg_data_etl import Database
 
 
-class NetworkNode:
-    def __init__(self, uid: int, db: PostgreSQL = _db):
-        self.uid = uid
+class NetworkNodes:
+    """
+    This class exists to stitch a new set of edges into
+    a pre-existing network of edges.
+
+    Using the new edge's nodes, draw new connector lines to
+    the old network's nodes.
+    """
+
+    def __init__(
+        self,
+        db: Database,
+        new_nodes: str,
+        new_node_uid_col: str,
+        old_nodes: str,
+        old_node_uid_col: str,
+    ):
         self.db = db
+        self.new_nodes = new_nodes
+        self.old_nodes = old_nodes
+        self.new_node_uid_col = new_node_uid_col
+        self.old_node_uid_col = old_node_uid_col
 
-    def nearby_nodes(self, search_dist: float) -> list:
+    def new_node_uids(self, table: str):
+        if table == "new":
+            col = self.new_node_uid_col
+            tbl = self.new_nodes
+
+        elif table == "old":
+            col = self.new_node_uid_col
+            tbl = self.new_nodes
+
+        else:
+            return None
+
+        return db.query_as_list_of_singletons(f"select {col} from {tbl};")
+
+    def nearby_nodes(self, uid: int, search_dist: float) -> list:
         """Get list of all nodes within search_dist"""
-        query = f""" """
+        query = f"""
+        
+        """
         pass
 
     def nearest_node(self):
@@ -24,3 +55,11 @@ class NetworkNode:
     def connected_segments(self) -> list:
         """Get a list of all segments that intersect this node"""
         pass
+
+
+if __name__ == "__main__":
+    db = pg_db_connection()
+
+    nn = NetworkNodes(
+        db, "improvements.montco_new_nodes", "node_id", "nodes_for_sidewalks", "sw_node_id"
+    )
