a4909aca1eaf752f6783088eef85f100bf4dff4f
aaronfraint <aaronfraint@icloud.com>
2021-07-19 14:23:14 -0400

    add docstrings

--- network_routing/improvements/network_node.py
+++ network_routing/improvements/network_node.py
@@ -1,11 +1,14 @@
 """
 """
+from __future__ import annotations
+
 import click
 import pandas as pd
 from tqdm import tqdm
 
-from network_routing import pg_db_connection
 from pg_data_etl import Database
+
+from network_routing import pg_db_connection
 
 
 @click.command()
@@ -41,6 +44,17 @@ class NetworkNodes:
 
     Using the new edge's nodes, draw new connector lines to
     the old network's nodes.
+
+    This process can also be used to draw connector lines within a single table
+
+    Parameters:
+        db (Database): analysis database
+        new_nodes (str): name of the table with the 'new' nodes
+        new_node_uid_col  (str): name of the column in the new table that has unique IDs
+        old_nodes (str): name of the table with the 'old' nodes
+        old_node_uid_col  (str): name of the column in the old table that has unique IDs
+        new_edges (str): name of the 'new' edge table
+        old_edges (str): name of the 'old' edge table
     """
 
     def __init__(
@@ -63,7 +77,16 @@ class NetworkNodes:
             "old": old_edges,
         }
 
-    def node_uids(self, table: str):
+    def node_uids(self, table: str) -> list:
+        """
+        - Get a list of every unique ID within the 'new' or 'old' node table
+
+        Arguments:
+            table (str): key for the table to use. Options include 'new' and 'old'
+
+        Returns:
+            list: with each item in the list being a unique ID for the table
+        """
 
         values = self.nodes[table]
 
@@ -73,11 +96,26 @@ class NetworkNodes:
         return self.db.query_as_list_of_singletons(f"select {col} from {tbl};")
 
     def nearby_nodes(
-        self, uid: int, n: int = 1, search_dist: float = 25.0, src: str = "new", dest: str = "old"
-    ) -> list:
+        self,
+        uid: int,
+        src: str = "new",
+        dest: str = "old",
+        n: int = 1,
+        search_dist: float = 25.0,
+    ) -> list | None:
         """
         - Get the ID of the n-closest node within search_dist
         - If src and dest are the same table, choose the closest node that does not share the unique ID
+
+        Arguments:
+            uid (int): the unique ID of the node to analyze
+            src (str): key for the table to use. Options include 'new' and 'old'
+            dest (str): key for the table to use. Options include 'new' and 'old'
+            n (int): number of results to return. Defaults to 1
+            search_dist (float): map units for search tolerance. Defaults to 25 (meters)
+
+        Returns:
+            list | None: list with N-nearest node IDs, or None if there aren't any
         """
         query = f"""
             with src as (
@@ -104,16 +142,25 @@ class NetworkNodes:
             limit {n}        
         """
 
-        result = self.db.query(query)
+        result = self.db.query_as_list_of_singletons(query)
+
         if result:
-            return result[n - 1][0]
+            return result
         else:
             return None
 
     def uids_to_analyze(self, table: str = "new") -> list:
         """
         - Figure out which uids should be analyzed
+        - Find all nodes that intersect only one edge line, and return unique ID value for each
+
+        Arguments:
+            table (str): key for the table to use. Options include 'new' and 'old'
+
+        Returns:
+            list: that includes unique ID values of each node on a dangling network edge
         """
+
         query = f"""
             select
                 nodes.{self.nodes[table]['col']}
@@ -130,7 +177,23 @@ class NetworkNodes:
         """
         return self.db.query_as_list_of_singletons(query)
 
-    def draw_lines(self, from_table: str = "new", to_table: str = "old"):
+    def draw_lines(self, from_table: str = "new", to_table: str = "old", n: int = 1) -> None:
+        """
+        - Draw straight line from the 'from_table' nodes to the 'new_table' nodes
+        - Steps:
+            1) find dangling nodes
+            2) identify N-nearest node ids for each result of #1
+            3) draw lines between all results from #2
+            4) save result of #3 to database
+
+        Arguments:
+            from_table (str): key for the table to use. Options include 'new' and 'old'
+            to_table (str): key for the table to use. Options include 'new' and 'old'
+            n (int): number of nearest nodes to find/draw lines for. Defaults to 1
+
+        Returns:
+            None: but writes result to new spatial table in database
+        """
         print(f"Handling {from_table.upper()} nodes to {to_table.upper()} nodes")
 
         from_tablename = self.nodes[from_table]["tbl"]
@@ -146,9 +209,9 @@ class NetworkNodes:
         print("Finding src nodes with dest nodes nearby")
         for src_uid in tqdm(uids_to_analyze, total=len(uids_to_analyze)):
 
-            dest_uid = self.nearby_nodes(src_uid, src=from_table, dest=to_table)
+            dest_uid_list = self.nearby_nodes(src_uid, src=from_table, dest=to_table, n=n)
 
-            if dest_uid:
+            for dest_uid in dest_uid_list:
                 flows[src_uid] = dest_uid
 
         all_results = []
@@ -182,19 +245,3 @@ class NetworkNodes:
         self.db.import_geodataframe(
             merged_gdf, output_tablename, gpd_kwargs={"if_exists": "replace"}
         )
-
-
-# if __name__ == "__main__":
-#     db = pg_db_connection()
-
-#     nn = NetworkNodes(
-#         db,
-#         "improvements.montco_new_nodes",
-#         "node_id",
-#         "nodes_for_sidewalks",
-#         "sw_node_id",
-#         "improvements.montgomery_split",
-#         "pedestriannetwork_lines",
-#     )
-
-#     nn.draw_lines()
