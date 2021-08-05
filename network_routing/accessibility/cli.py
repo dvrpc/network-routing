e046c5428b6b4df481604fde1e1ca3d295698bc0
aaronfraint <aaronfraint@icloud.com>
2021-08-05 17:30:09 -0400

    update access score processes

--- network_routing/accessibility/cli.py
+++ network_routing/accessibility/cli.py
@@ -98,15 +98,20 @@ def sw_default():
 
 
 @click.command()
-def osm_ridescore():
-    """Analyze OSM network distance around each rail stop """
+def osm_access_score():
+    """
+    Analyze OSM network distance around each rail stop
+
+    This uses an OSM network that INCLUDES highways.
+    """
 
     arguments = {
         "poi_table_name": "ridescore_pois",
         "poi_id_column": "dvrpc_id",
         "output_table_name": "osm",
-        "output_schema": "rs_osm",
+        "output_schema": "access_score_osm",
         "num_pois": 1,
+        "max_minutes": 50,  # 48 minutes = 2 miles
         "poi_match_threshold": 152,  # aka 500'
         "edge_table_name": "osm_edges_all_no_motorway",
         "node_table_name": "nodes_for_osm_all",
@@ -117,15 +122,41 @@ def osm_ridescore():
 
 
 @click.command()
-def sw_ridescore():
-    """Analyze sidewalk network distance around each rail stop """
+def lowstress_access_score():
+    """
+    Analyze network distance around each rail stop
+    using the low-stress bicycle network
+    """
+
+    arguments = {
+        "poi_table_name": "ridescore_pois",
+        "poi_id_column": "dvrpc_id",
+        "output_table_name": "lts",
+        "output_schema": "access_score_lts",
+        "num_pois": 1,
+        "max_minutes": 50,  # 48 minutes = 2 miles
+        "poi_match_threshold": 152,  # aka 500'
+        "edge_table_name": "lowstress_islands",
+        "node_table_name": "nodes_for_lowstress_islands",
+        "node_id_column": "node_id",
+    }
+
+    _ = _execute_analysis_into_one_output(arguments)
+
+
+@click.command()
+def sw_access_score():
+    """
+    Analyze sidewalk network distance around each rail stop
+    """
 
     arguments = {
         "poi_table_name": "ridescore_pois",
         "poi_id_column": "dvrpc_id",
         "output_table_name": "sw",
-        "output_schema": "rs_sw",
+        "output_schema": "access_score_sw",
         "num_pois": 1,
+        "max_minutes": 50,  # 48 minutes = 2 miles
         "poi_match_threshold": 152,  # aka 500'
         "edge_table_name": "pedestriannetwork_lines",
         "node_table_name": "nodes_for_sidewalks",
@@ -215,8 +246,9 @@ def eta_individual(county):
 
 _all_commands = [
     sw_default,
-    osm_ridescore,
-    sw_ridescore,
+    osm_access_score,
+    lowstress_access_score,
+    sw_access_score,
     sw_eta,
     osm_eta,
     eta_individual,
