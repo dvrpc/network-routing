eaaf26168f6751a32a1728c58978566d99083a1f
aaronfraint <aaronfraint@icloud.com>
2022-02-04 11:10:58 -0500

    remove level_0 column from explosion

--- network_routing/database/setup/setup_00_initial.py
+++ network_routing/database/setup/setup_00_initial.py
@@ -103,6 +103,13 @@ def setup_00_initial(local_db: Database):
 
     local_db.import_geodataframe(stops, "regional_transit_stops", explode=True)
 
+    db.execute(
+        """
+        alter table regional_transit_stops 
+        drop column level_0;
+    """
+    )
+
     # 4) Import OSM data for the entire region
     import_osm_for_dvrpc_region(local_db, network_type="all")
 
